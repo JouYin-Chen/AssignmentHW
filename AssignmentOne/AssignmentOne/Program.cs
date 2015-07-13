7b80835334f5b775e84b03a9d3d93ae0d90ab280
unknown <zoechen@artsztu-win7.inhouse.moneydj.com>
2015-07-13 16:44:56 +0800

    One

---
+++ AssignmentOne/AssignmentOne/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String FirstName = "Tom";
+            String LastName = "Lusis";
+            String Birthdate = "1991/1/23";
+            String AddressLine1 = "蓮海路";
+            String AddressLine2 = "鼓山區";
+            String City = "高雄市";
+            String State_Province = "台灣省";
+            int Zip_Postal = 70;
+            String Country = "Taiwan";
+
+            Console.WriteLine("Name : {0} {1}\r\n",FirstName, LastName);
+            Console.WriteLine("Birthdate : {0}\r\n",Birthdate);
+            Console.WriteLine("Address : " + AddressLine1 + AddressLine2 + City + State_Province + " No." + Zip_Postal + " " + Country+"\r\n");
+            Console.ReadKey();
+        }
+    }
+}
